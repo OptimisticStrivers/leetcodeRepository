cf489fb48b3107775ca22714df7b8883cf753d16
Cc1134 <chenxinyicobby@gmail.com>
2022-05-06 22:10:27 +0930

    lc 2 & 3
    review

--- src/main/java/edu/cmu/optimisticStrivers/linkedList/CXY_2_addTwoNumbers_medium.java
+++ src/main/java/edu/cmu/optimisticStrivers/linkedList/CXY_2_addTwoNumbers_medium.java
@@ -1,2 +1,54 @@
-package edu.cmu.optimisticStrivers.linkedList;public class CXY_2_addTwoNumbers_medium {
+package edu.cmu.optimisticStrivers.linkedList;
+
+public class CXY_2_addTwoNumbers_medium {
+    // Definition for singly-linked list.
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode head = null;
+        ListNode curr = null;
+        int ans = 0;
+        boolean carryDigit = false;
+        while(l1 != null || l2 != null){
+            ans = ((l1==null)?0:l1.val) + ((l2==null)?0:l2.val) + ((carryDigit)?1:0);
+            if(ans>=10){
+                ans = ans%10;
+                carryDigit = true;
+            }else {
+                carryDigit=false;
+            }
+            if(head==null){
+                head = new ListNode(ans);
+                curr = head;
+            }else {
+                curr.next = new ListNode(ans);
+                curr = curr.next;
+            }
+            if(l1 != null){
+                l1 = l1.next;
+            }
+            if(l2 != null){
+                l2 = l2.next;
+            }
+        }
+        if(carryDigit){
+            curr.next = new ListNode(1);
+        }
+        return head;
+    }
 }
--- src/main/java/edu/cmu/optimisticStrivers/linkedList/CXY_3_LongestSubstring_medium.java
+++ src/main/java/edu/cmu/optimisticStrivers/linkedList/CXY_3_LongestSubstring_medium.java
@@ -1,2 +1,33 @@
-package edu.cmu.optimisticStrivers.linkedList;public class CXY_3_LongestSubstring_medium {
+package edu.cmu.optimisticStrivers.linkedList;
+
+import java.util.HashSet;
+
+public class CXY_3_LongestSubstring_medium {
+    public int lengthOfLongestSubstring(String s) {
+        if(s.length() == 0){
+            return 0;
+        }
+
+        int start = 0;
+        int end = 1;
+        int maxlen = 1;
+        HashSet<Character> set = new HashSet<>();
+        set.add(s.charAt(start));
+        while (true) {
+            while (end < s.length() && (!(set.contains(s.charAt(end))))) {
+                set.add(s.charAt(end));
+                end++;
+            }
+            if(maxlen<(end-start)){
+                maxlen = end-start;
+            }
+            if(end>=s.length()){
+                return maxlen;
+            }
+            while (set.contains(s.charAt(end))) {
+                set.remove(s.charAt(start));
+                start++;
+            }
+        }
+    }
 }
