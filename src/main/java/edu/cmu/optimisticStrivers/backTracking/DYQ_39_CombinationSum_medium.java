2d3ca527f73c54c78acf0a07acd991a228f768a3
duyuqi.tim <duyuqi.tim@bytedance.com>
2022-07-21 17:43:59 +0800

    combinationSum

--- src/main/java/edu/cmu/optimisticStrivers/backTracking/DYQ_39_CombinationSum_medium.java
+++ src/main/java/edu/cmu/optimisticStrivers/backTracking/DYQ_39_CombinationSum_medium.java
@@ -10,32 +10,55 @@ import java.util.*;
  * @Version 1.0
  */
 public class DYQ_39_CombinationSum_medium {
-    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+//    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+//
+//        List<List<Integer>> res = new ArrayList<>();
+//        Deque<Integer> path = new ArrayDeque<>();
+//        Arrays.sort(candidates);
+//        dfs(candidates,target,res,path,0,0);
+//        return res;
+//    }
+//
+//    //剪枝当然是好的！！！
+//    private void dfs(int[] candidates, int target, List<List<Integer>> res, Deque<Integer> path, int currentSum, int start) {
+//        if(currentSum == target) {
+//            res.add(new ArrayList<>(path));
+//            return;
+//        }
+//        //这个剪枝没啥用，因为下面的已经剪了
+////        if(currentSum > target){
+////            return; //剪枝
+////        }
+//
+//        for (int i = start; i < candidates.length; i++) {
+//            if(currentSum+candidates[i]>target) return; //剪枝
+//            path.addLast(candidates[i]);
+//            // dfs 里面 start 是 i， 因为选过的可以再选
+//            dfs(candidates,target,res,path,currentSum+candidates[i],i);
+//            path.removeLast();
+//        }
+//    }
 
+
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
         List<List<Integer>> res = new ArrayList<>();
-        Deque<Integer> path = new ArrayDeque<>();
-        Arrays.sort(candidates);
-        dfs(candidates,target,res,path,0,0);
+        Stack<Integer> stack = new Stack<>();
+//        Arrays.sort(candidates); 这个无所谓 因为这道题 candidates里面的元素都是唯一
+        dfs(res, candidates, 0, target, stack, 0 );
         return res;
     }
 
-    //剪枝当然是好的！！！
-    private void dfs(int[] candidates, int target, List<List<Integer>> res, Deque<Integer> path, int currentSum, int start) {
-        if(currentSum == target) {
-            res.add(new ArrayList<>(path));
+    private void dfs(List<List<Integer>> res, int[] candidates, int curSum, int target, Stack<Integer> stack, int begin) {
+        if (curSum > target) return;
+        if (curSum == target) {
+            res.add(new ArrayList<>(stack));  //需要硬拷贝 不能软拷贝
             return;
         }
-        //这个剪枝没啥用，因为下面的已经剪了
-//        if(currentSum > target){
-//            return; //剪枝
-//        }
-
-        for (int i = start; i < candidates.length; i++) {
-            if(currentSum+candidates[i]>target) return; //剪枝
-            path.addLast(candidates[i]);
-            // dfs 里面 start 是 i， 因为选过的可以再选
-            dfs(candidates,target,res,path,currentSum+candidates[i],i);
-            path.removeLast();
+        for (int i = begin; i < candidates.length; i++) {
+            stack.push(candidates[i]);
+            dfs(res, candidates, curSum + candidates[i], target, stack, i);
+            stack.pop();
         }
     }
+
 }
